c157fa840b18cdb7fb44eec45c3bb3e7c5166b64
rachel etnire and tim devlin <tim@>
2016-10-03 19:35:20 -0700

    Added Front end logic and firstspec test

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-$(function() {
+// Back-End Logic
 
+var leapYear = function(year) {
+  return false;
+};
+
+
+// Front-End Logic
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
 });
